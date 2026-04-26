1b3be95cc494b41bfdc9a688ecc1be4f15f8e590
Nathaniel <code.s9gdr@simplelogin.co>
2026-04-26 09:22:11 -0500

    Add aditional fixes

--- fixup_text.py
+++ fixup_text.py
@@ -15,8 +15,30 @@ def fixup(s: str):
     from utils import colors
     import re
 
-    def replace(match):
-        return f' {colors.RED}{match.group(0).strip()}{colors.ENDC} '
+    def replace(match: re.Match):
+        # return f' {colors.RED}{match.group(1)}{colors.ENDC} '
+        return f' {match.group(1)} '
+    def add_space_after(match: re.Match):
+        return f'{match.group(1)} '
+    def add_space_before(match: re.Match):
+        return f' {match.group(1)}'
+    def identity(match: re.Match):
+        return match.group(1)
 
-    pattern = r' ?(' + '|'.join(re.escape(w) for w in words) +r') ?'
-    return re.sub(pattern, replace, s)
+    misc_fixes = [
+        (r'ona', 'on a'),
+        (r'ina', 'in a'),
+        (r'([,:\.])', add_space_after), # , . :
+        (r'([\(])', add_space_before), # (
+        (r'[ \u0001]+', ' '), # Remove duplicate spaces
+        (r' ([\.,:])', identity), # Remove spaces in front
+        (r'([\(]) ', identity), # Remove spaces after
+        (r'\) :', '):'), # "Save (5) :" -> "Save (5):"
+    ]
+
+    pattern = r'(' + '|'.join(re.escape(w) for w in words) +r')'
+    s = re.sub(pattern, replace, s)
+
+    for fix in misc_fixes:
+        s = re.sub(fix[0], fix[1], s)
+    return s.strip()
