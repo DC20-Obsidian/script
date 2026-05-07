07c5a2b584e17028c82cec2129336673c6c2cdcf
Nathaniel <code.s9gdr@simplelogin.co>
2026-05-06 19:38:27 -0500

    Cleanup ancestry type

--- dc_types/ancestry.py
+++ dc_types/ancestry.py
@@ -2,7 +2,7 @@ import re
 from functools import reduce
 from dc_types.proto_item import DCProtoItem
 from dc_types.frag_list import FragList
-from utils.split import split_items
+from utils.split import split_items_full, SplitPrams, SplitBuilder
 from .item import Item
 from typing import Self, Optional
 
@@ -12,6 +12,7 @@ class Ancestry(Item):
         self._type = "ancestry"
         self.name: str = ""
         self.page: int = -1
+        self.description: str = ""
         self.traits: list[Trait] = []
 
     @classmethod
@@ -20,6 +21,7 @@ class Ancestry(Item):
         a.name = d["name"]
         a.page = int(d["page"])
         a.traits = d["traits"]
+        a.description = d["description"]
         return a
 
     @classmethod
@@ -35,13 +37,18 @@ class Ancestry(Item):
 
     @classmethod
     def split(cls, frags: FragList) -> list[DCProtoItem]:
-        return split_items(frags, ["f4"], ["f3"], 16.5, ["f2", "f9", "f1"], [], [])
+        prams: SplitPrams = SplitBuilder(
+            is_header=["f4"],
+            cont_item=["beasttraits"],
+        ).build()
+        return split_items_full(frags, prams)
 
     def markdown(self) -> str:
         args = {
             "name": self.name,
             "page": self.page,
             "traits": reduce(lambda s, t: s + t.markdown(), self.traits, ""),
+            "description": self.description,
         }
         raise Exception("Not yet implimented")
         return template.format(**args)
@@ -55,39 +62,54 @@ class Trait(Item):
         self._type = "ancestry_trait"
         self.name: str = ""
         self.page: int = -1
-        self.cost: int = -1
+        self.cost: int = -100
         self.dependent_on: Optional[str] = None
-        self.description: str = "1"
+        self.description: str = ""
         self.default: bool = False
         self.kind: str = ""
+        self.ancestry: str = ""
 
-        @classmethod
-        def from_json(cls, d: dict) -> Self:
-            t: Trait = cls()
-            t.name = d["name"]
-            t.page = d["page"]
-            t.cost = int(d["cost"])
-            t.dependent_on = d["dependent_on"]
-            t.description = d["description"]
-            t.default = d["default"]
-            t.kind = d["kind"]
-            return t
+    @classmethod
+    def from_json(cls, d: dict) -> Self:
+        t: Trait = cls()
+        t.name = d["name"]
+        t.page = d["page"]
+        t.cost = int(d["cost"])
+        t.dependent_on = d["dependent_on"]
+        t.description = d["description"]
+        t.default = d["default"]
+        t.kind = d["kind"]
+        t.ancestry = d["ancestry"]
+        return t
 
-        def fixup(self) -> Self:
-            return self
+    def fixup(self) -> Self:
+        return self
 
-        def markdown(self) -> str:
-            args = {
-                "name": self.name,
-                "page": self.page,
-                "cost": self.cost,
-                "dependent_on": self.dependent_on,
-                "description": self.description,
-                "default": self.default,
-                "kind": self.kind,
-            }
-            return trait_template.format(**args)
+    def markdown(self) -> str:
+        args = {
+            "name": self.name,
+            "page": self.page,
+            "cost": self.cost,
+            "dependent_on": self.dependent_on,
+            "description": self.description,
+            "default": self.default,
+            "kind": self.kind,
+            "ancestry": self.ancestry,
+        }
+        return trait_template.format(**args)
 
+    @classmethod
+    def get_default_page_range(cls) -> slice[int, int]:
+        raise Exception("Not implimented")
+
+    @classmethod
+    def get_save_file(cls, data_folder: str, version: str) -> str:
+        (_, _) = (data_folder, version)
+        raise Exception("Not implimented")
+
+    def markdown_path(self, prefix: str) -> str:
+        _ = prefix
+        raise Exception("Not implimented")
 
 template: str = """ TODO
 """
